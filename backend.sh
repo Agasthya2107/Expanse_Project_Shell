aada9b750fd4fe15602e02f8aa2e147962174b61
Agasthya <mshyamala15@gmail.com>
2025-05-22 13:42:21 -0400

    Backend data Configured

--- backend.sh
+++ backend.sh
@@ -56,7 +56,7 @@ then
         echo -e "$R User already exists no need to create again"
 fi
 
-mkdir -/app &>>$LOG_FILE_NAME
+mkdir -p /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creating New APP Directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
